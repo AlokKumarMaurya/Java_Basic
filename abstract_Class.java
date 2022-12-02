0999801c14be29ba50b0bf106712ee14da833bee
Alok Kumar Maurya <alokkumarmaurya5556@gmail.com>
2022-12-02 22:25:40 +0530

    Create abstract_Class.java

---
+++ abstract_Class.java
@@ -0,0 +1,17 @@
+abstract class Test{
+   abstract public void test_Abstract_Method();
+}
+
+
+class test1 extends Test{
+  public void  test_Abstract_Method(){
+       System.out. print("test");
+    }
+}
+
+
+class Main{
+    public static void main(String [] asd){
+        System.out.print("success");
+    }
+}
