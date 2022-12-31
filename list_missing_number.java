fc5761285f51a41f22cf99f7f315ff4474959260
Alok Kumar Maurya <alokkumarmaurya5556@gmail.com>
2022-12-31 08:31:58 +0530

    Create list_missing_number.java

---
+++ list_missing_number.java
@@ -0,0 +1,25 @@
+class Main{
+    public static void main(String [] ernfg){
+        testfn();
+    }
+    
+    public static void   testfn(){
+    int[]  a={1,2,3,4,5,6,7,8,9,0};
+    boolean temp=false;
+    for(int i=0;i<=8;i++){    
+        for(int j=0;j<a.length;j++){
+            if(a[j]==i){
+                temp=true;
+                break;
+            }else temp=false;
+        }
+        if(temp==false){
+              System.out.println("wefgrewqdfewdsdfedwdef");
+            break;
+        }
+    }
+    System.out.println(temp);
+}
+}
+
+
